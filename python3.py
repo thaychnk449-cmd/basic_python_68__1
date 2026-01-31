1f8aa8936ccd4b8ba006bedefa85bf5f982368da
Thanchanok <thaychnk449>
2026-01-31 14:16:49 +0700

    Collection of Python

---
+++ python3.py
@@ -0,0 +1,52 @@
+""""
+#
+# Part: Collection of Python
+# Lists, Tuples, Sets, Dictionaries
+#
+"""
+"""
+#
+# Lists
+#
+"""
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_list)
+print(this_list[1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+# Tuples
+#
+"""
+this_tuple = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_tuple)
+print(type(this_tuple))
+print(len(this_tuple))
+
+"""
+#
+# Sets
+#
+"""
+this_set = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_set)
+print(this_set[1])
+print(type(this_set))
+print(len(this_set))
+
+"""
+#
+# Dictionaries
+#
+"""
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(this_dict)
+print(this_dict["model"])
+print(type(this_dict))
+print(len(this_dict))
