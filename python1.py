0b87839073a15504f9581e34fcbe407f9e44896e
Thanchanok <thaychnk449>
2026-01-31 13:18:33 +0700

    Python Variables

---
+++ python1.py
@@ -0,0 +1,33 @@
+# """"
+# # 
+# # Part: Python Comment
+# # 
+
+# """"
+# v = s / t
+# ความเร็ว = ระยะทาง / เวลา
+# v = ความเร็ว (m/s)
+# s = ระยะทาง (m)
+# t = เวลา (s)
+# """"
+
+print("Hello, World!")
+
+x = 10
+y = "10"
+print(x, y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400
+MYVAR = 500
+
+myVariableName = 900
+myVariableName = 1000
+my_variable_Name = 1100
