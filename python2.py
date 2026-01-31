dffffb44b2a1cc1bb880fd2fae219a63532d5042
Thanchanok <thaychnk449>
2026-01-31 14:15:39 +0700

    Python Operators

---
+++ python2.py
@@ -0,0 +1,39 @@
+# """
+#
+# Part: Python Operators
+# Arithmetic Operators
+#
+# """
+x = 4 + 5
+y = 10 - 2
+z = 3 * 7
+a = 8 / 2
+b = 10 % 3
+c = 2 ** 3
+
+x = 5
+x += 3
+x = x + 3
+
+#comparison
+a = 5
+b = 10
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+#Logic
+x = True
+y = False
+print(x and y)
+print(x or y)
+print(not x)
+print(x & y)
+print(x | y)
+print(~x)
+a = 5
+b = 10
+print((a < 10) and (b > 5))
