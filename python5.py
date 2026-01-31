5ffa170f71c319cde53ce9372163d60c08a4f03d
Thanchanok <thaychnk449>
2026-01-31 14:39:18 +0700

    Loop

---
+++ python5.py
@@ -0,0 +1,23 @@
+# """
+#
+# Loop
+# while Loop
+#
+# """
+i = 1
+while i < 5:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+#
+# Loop
+# For Loop
+#
+# """
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "banana":
+        break
